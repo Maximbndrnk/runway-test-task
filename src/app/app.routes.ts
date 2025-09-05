7e90afe482fda82e2eb438ac85d12735c0cf8b71
max-bondarenko-evo <max.bondarenko@evo-studio.com>
2025-09-05 09:57:55 +0300

    add some styles to improve UI look

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'users' },
+  {
+    path: 'users',
+    loadComponent: () =>
+      import('./users/users-list/users-list.component').then(m => m.UsersListComponent)
+  },
+  {
+    path: 'users/new',
+    loadComponent: () =>
+      import('./users/user-edit/user-edit.component').then(m => m.UserEditComponent)
+  },
+  {
+    path: 'users/:id',
+    loadComponent: () =>
+      import('./users/user-detail/user-detail.component').then(m => m.UserDetailComponent)
+  },
+  {
+    path: 'users/:id/edit',
+    loadComponent: () =>
+      import('./users/user-edit/user-edit.component').then(m => m.UserEditComponent)
+  },
+  { path: '**', redirectTo: 'users' }
+];
